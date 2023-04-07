fb12f9604d56948e3cc0a90e0763495406dbf3fa
Everton Cabral <90525060+Everton-Cabral@users.noreply.github.com>
2023-04-07 10:16:27 -0300

    Inicializando o projeto e criando o componente TopBar

---
+++ src/components/TopBar.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="c-topbar">
+        <h1>Where in the world?</h1>
+        <span>
+            <ion-icon name="moon-outline"></ion-icon>
+            Dark Mode
+        </span>
+
+    </div>
+</template>
+
+<script>
+export default {
+    name:'TopBar',
+
+}
+</script>
+
+<style>
+.topbar{
+
+}
+</style>
